19c9ff45db43c40325277e5ce9378efe5f206fc4
Jakub Gargul <jakub.gargul@nsn.com>
2015-04-01 00:12:11 +0200

    slave preparation

--- slave/main.cpp
+++ slave/main.cpp
@@ -1,32 +1,31 @@
 #include <mpi.h>
 #include <iostream>
 #include <string>
+#include <vector>
+
+const int segmentSize(int rank, long endNum)
+{
+    return endNum / rank;
+}
 
 int main(int argc, char* argv[])
 {
     MPI_Init(&argc, &argv);
 
-    int rank;
+    int rank, numOfJobs;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &numOfJobs);
+
     if (rank == 0)
     {
-        int value = std::stoi(argv[1]);
-        int result = MPI_Bcast(&value, 1, MPI_INT, 0, MPI_COMM_WORLD);
-        if (result == MPI_SUCCESS)
-            std::cout << "Rank 0 OK!" << std::endl;
-        else
-            std::cout << "Rank 0 blew up!" << std::endl;
-    } else if (rank > 0)
+        long value = std::stoi(argv[1]);
+        int result = MPI_Bcast(&value, 1, MPI_LONG, 0, MPI_COMM_WORLD);
+    }
+    else if (rank > 0)
     {
-        int value;
-        int result = MPI_Bcast(&value, 1, MPI_INT, 0, MPI_COMM_WORLD);
-        if (result == MPI_SUCCESS && value == 17)
-            std::cout << "Rank " << rank << " OK!" << std::endl;
-        else
-        {
-            std::cout << "Rank " << rank << " blew up!" << std::endl;
-            return 0;
-        }
+        long value;
+        int result = MPI_Bcast(&value, 1, MPI_LONG, 0, MPI_COMM_WORLD);
+        
     }
     MPI_Finalize();
     return 0;
