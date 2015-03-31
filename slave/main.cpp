7b786ed17bbc2b9316ba6e1d6d22166fdbcd5ec9
Jakub Gargul <jakub.gargul@nsn.com>
2015-03-31 23:19:19 +0200

    Bcast usage

--- slave/main.cpp
+++ slave/main.cpp
@@ -8,23 +8,24 @@ int main(int argc, char* argv[])
 
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    if (rank == 0) {
+    if (rank == 0)
+    {
         int value = std::stoi(argv[1]);
-        int result = MPI_Send(&value, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+        int result = MPI_Bcast(&value, 1, MPI_INT, 0, MPI_COMM_WORLD);
         if (result == MPI_SUCCESS)
             std::cout << "Rank 0 OK!" << std::endl;
         else
             std::cout << "Rank 0 blew up!" << std::endl;
-    } else if (rank == 1) {
+    } else if (rank > 0)
+    {
         int value;
-        int result = MPI_Recv(&value, 1, MPI_INT, 0, 0, MPI_COMM_WORLD,
-                              MPI_STATUS_IGNORE);
+        int result = MPI_Bcast(&value, 1, MPI_INT, 0, MPI_COMM_WORLD);
         if (result == MPI_SUCCESS && value == 17)
-            std::cout << "Rank 1 OK!" << std::endl;
+            std::cout << "Rank " << rank << " OK!" << std::endl;
         else
         {
-            std::cout << "Rank 1 blew up!" << std::endl;
-            return 42;
+            std::cout << "Rank " << rank << " blew up!" << std::endl;
+            return 0;
         }
     }
     MPI_Finalize();
